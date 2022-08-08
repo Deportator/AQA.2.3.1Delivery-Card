f09d1e9cc1c2906d6665ba87ed4cf54a9f449675
Deportator <fabrichnov87@gmail.com>
2022-08-08 17:25:29 +0300

    First task. Ordering card delivery

--- src/test/java/ru/netology/test/CardTest.java
+++ src/test/java/ru/netology/test/CardTest.java
@@ -14,6 +14,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.BACK_SPACE;
 
 public class CardTest {
 
@@ -30,20 +31,17 @@ public class CardTest {
     void shouldOrderDeliveryCard() {
 
         $("[placeholder='Город']").setValue("Санкт-Петербург");
-        $("[data-test-id=date] .input__icon").click();
-        LocalDate threeDays = LocalDate.now().plusDays(4);
-        LocalDate inputWeek = LocalDate.now().plusDays(6);
-        if (threeDays.getMonthValue() != inputWeek.getMonthValue()) {
-            $("[data-step='1']").click();
-        }
-        $$("tr td").findBy(text(String.valueOf(inputWeek.getDayOfMonth()))).click();
+        $("[data-test-id=date] .input__control").click();
+        $("[data-test-id=date] .input__control").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id=date] .input__control").sendKeys(BACK_SPACE);
+        $("[data-test-id=date] .input__control").setValue(generateDate(7));
         $("[name='name']").setValue("Иванов-Петров Иван");
         $("[name='phone']").setValue("+79213523223");
         $("[class='checkbox__box']").click();
         $$("button").find(Condition.exactText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $(".notification__content")
-                .shouldHave(text("Встреча успешно забронирована на " + generateDate(6)), Duration.ofSeconds(15))
+                .shouldHave(text("Встреча успешно забронирована на " + generateDate(7)), Duration.ofSeconds(15))
                 .shouldBe(visible);
     }
 }
